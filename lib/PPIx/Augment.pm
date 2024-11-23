94614e2627549873a26df69f5df222c584485100
Branislav Zahradník <barney@cpan.org>
2024-11-23 21:36:24 +0100

    Basic project setup - Dist::Zilla, .gitignore, .github/workflows

---
+++ lib/PPIx/Augment.pm
@@ -0,0 +1,32 @@
+
+use v5.14;
+use warnings;
+
+package PPIx::Augment {
+	1;
+}
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+PPIx::Augment - Augment structure of PPI document
+
+=head1 DESCRIPTION
+
+L<PPIx::Augment> extends L<PPI> data model with focus to simplify
+automated source code processing (especially refactoring).
+
+=head1 AUTHOR
+
+Branislav Zahradník <barney@cpan.org>
+
+=head1 COPYRIGHT AND LICENCE
+
+PPIx::Augment distribution is distributed under Artistic Licence 2.0.
+
+=cut
